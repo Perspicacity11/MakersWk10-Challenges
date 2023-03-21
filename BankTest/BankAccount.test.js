d5488b1969cb4eab0f170793e99fafb3ae4fbe53
Cassius Naylor <cassiusnaylor@MASTUDENT-Cassius-Naylor-C02G5H2RQ6L4.local>
2023-03-21 13:40:09 +0000

    3. Second test (readBalance) fails

--- BankTest/BankAccount.test.js
+++ BankTest/BankAccount.test.js
@@ -5,4 +5,9 @@ describe('Bank Account', () => {
         let testAccount = new BankAccount();
         expect(testAccount.balance).toBe(0)
     })
+
+    it('Instantiates a new bank account and reads balance at 0; tests read balance', () => {
+        let testAccount = new BankAccount();
+        expect(testAccount.readBalance()).toBe(0)
+    })
 })
